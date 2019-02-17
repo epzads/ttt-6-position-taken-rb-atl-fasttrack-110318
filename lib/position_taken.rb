b25fdbfdc0c41191176301e51c7b8ee97186041f
favorable-vector-1743 <epzads@gmail.com>
2019-02-17 18:00:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken? ( board,index )
-if index == " "
-
+if board[index] == " " || board[index] == ""
+ 
   return false
 end
 if board[index] == "X" || board[index] == "O"
